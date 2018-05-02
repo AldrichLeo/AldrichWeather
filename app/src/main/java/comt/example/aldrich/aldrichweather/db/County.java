416ee19511b6d02078ad7014aa80822d42f7a321
aldrich <646983414@qq.com>
2018-05-02 10:28:38 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/comt/example/aldrich/aldrichweather/db/County.java
@@ -0,0 +1,46 @@
+package comt.example.aldrich.aldrichweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Aldrich on 2018/5/2.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private int weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
---
+++ app/src/main/java/comt/example/aldrich/aldrichweather/db/Province.java
@@ -0,0 +1,37 @@
+package comt.example.aldrich.aldrichweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Aldrich on 2018/5/2.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
